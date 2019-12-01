7cad2372d7dd8d155d0dfbb414784d7f26ed8855
Faheem0007 <fah03134508455@gmail.com>
2019-12-01 15:47:48 +0500

    Header top Navbar Complete

---
+++ src/Containers/HeaderNav/HeaderNav.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { Menu, Form, Input, Icon, Image } from "semantic-ui-react";
+import "./HeaderNav.scss";
+
+class HeaderNav extends Component {
+  render() {
+    return (
+      <Menu borderless className="top-menu" fixed="top">
+        <Menu.Item className="logo">
+          <a href="/">EwigTube</a>
+        </Menu.Item>
+        <Menu.Menu className="nav-container">
+          <Menu.Item className="search-input">
+            <Form>
+              <Form.Field>
+                <Input placeholder="Search" size="small" action="Go" />
+              </Form.Field>
+            </Form>
+          </Menu.Item>
+          <Menu.Menu position="right">
+            <Menu.Item>
+              <Icon className="header-icon" name="video camera" size="large" />
+            </Menu.Item>
+            <Menu.Item>
+              <Icon className="header-icon" name="grid layout" size="large" />
+            </Menu.Item>
+            <Menu.Item>
+              <Icon className="header-icon" name="chat" size="large" />
+            </Menu.Item>
+            <Menu.Item>
+              <Icon className="header-icon" name="alarm" size="large" />
+            </Menu.Item>
+            <Menu.Item name="avatar">
+              <Image src="http://via.placeholder.com/80x80" avatar />
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu.Menu>
+      </Menu>
+    );
+  }
+}
+
+export default HeaderNav;
